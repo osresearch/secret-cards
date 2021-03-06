af90a6b9461dd1d2fe09e866760ec2cb26b41739
Trammell hudson <hudson@trmm.net>
2021-03-06 11:13:51 +0100

    index.js: send index.html

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const app = require('express')();
 const http = require('http').createServer(app);
 
 app.get('/', (req, res) => {
-	res.send('<h1>Hello world</h1>');
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname + "/index.html");
 });
 
 http.listen(4423, () => {
