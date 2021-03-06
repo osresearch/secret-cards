a4220b093dd42afbeac88fd6c07f4948b459014d
Trammell hudson <hudson@trmm.net>
2021-03-06 11:16:22 +0100

    npm add socket.io

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
 	//res.send('<h1>Hello world</h1>');
 	res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', (socket) => {
+	console.log('connected');
 });
 
 http.listen(4423, () => {
