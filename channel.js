f37b9f96b32a1e35edbf3ae8e1b22a57c59211c9
Trammell hudson <hudson@trmm.net>
2021-03-07 12:35:54 +0100

    make channel a class

--- channel.js
+++ channel.js
@@ -4,18 +4,6 @@
  * with the card table, and the dealing uses the socket io messages.
  */
 "use strict"
-var socket = io();
-let private_key = null;
-let public_key = null;
-let public_name = null;
-let public_seq = 0; // todo: should this be a random nonce?
-let players = {};
-
-const key_param = {
-	name: "ECDSA",
-	namedCurve: "P-384",
-	hash: { name: "SHA-256" },
-};
 
 function array2hex(bytes)
 {
@@ -42,224 +30,237 @@ function jwk2id(jwk)
 }
 
 
-function removeElement(id)
-{
-	let it = document.getElementById(id);
-	if (it)
-		it.remove();
-}
-
 function getEncoding(msg)
 {
 	return new TextEncoder().encode(msg);
 }
 
 
-// generate a signature on the msg, including the sequence number
-// and the public key used to sign it, and send it when it is ready
-function send_signed(dest,  msg)
+class SecureChannel
 {
-	let signed_msg = {
-		msg: msg,
-		seq: public_seq++,
-		key: public_name,
-	};
-
-	window.crypto.subtle.sign(
-		key_param,
-		private_key,
-		getEncoding(JSON.stringify(signed_msg))
-	).then((sig) => {
-		let sig_hex = array2hex(sig);
-		console.log(dest, signed_msg, sig, sig_hex);
-
-		// add the signature to the signed message and send it
-		signed_msg.sig = sig_hex;
-		socket.emit(dest, signed_msg);
-	});
-}
-
-
-/*
- * When the socket connects, attempt to register our key
- * with the server.
- */
-socket.on('connect', () => {
-	if (public_key)
+	constructor()
 	{
-		console.log("RECONNECTED");
-		socket.emit('register', public_key);
-		return;
+		this.socket = io();
+		this.private_key = null;
+		this.public_key = null;
+		this.public_name = null;
+		this.public_seq = 0; // todo: should this be a random nonce?
+		this.players = {};
+
+		this.key_param = {
+			name: "ECDSA",
+			namedCurve: "P-384",
+			hash: { name: "SHA-256" },
+		};
+
+		/*
+		 * When the socket connects, attempt to register our key
+		 * with the server.
+		 */
+		this.socket.on('connect', () => this.send_key());
+		this.socket.on('players', (player_list) => this.update_players(player_list));
 	}
 
-	console.log("FIRST CONNECT");
-	window.crypto.subtle.generateKey(
-		key_param,
-		true,
-		["sign", "verify"]
-	).then((k) => {
-		console.log("Key created", k);
-		private_key = k.privateKey;
-		return window.crypto.subtle.exportKey('jwk', k.publicKey);
-	}).then((jwk) => {
-		public_key = jwk;
-		public_name = jwk2id(jwk);
-		socket.emit('register', jwk);
-	});
-
-	// setup the chat box
-	let messages = document.getElementById('messages');
-	let form = document.getElementById('form');
-	let input = document.getElementById('input');
-
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
-		if (!input.value)
-			return;
-
-		send_signed('chat', input.value);
-		input.value = '';
-	});
-
-});
-
-
-// there is probably a better way to do this
-async function async_false() { return false }
-
-function recv_signed(msg)
-{
-	let player = players[msg.key];
-	if (!player)
+	// generate a signature on the msg, including the sequence number
+	// and the public key used to sign it, and send it when it is ready
+	// todo: is JSON.stringify() the best way to serialize the message?
+	emit(dest, msg)
 	{
-		console.log("UNKNOWN PLAYER:", msg);
-		return async_false();
+		let signed_msg = {
+			msg: msg,
+			seq: this.public_seq++,
+			key: this.public_name,
+		};
+
+		window.crypto.subtle.sign(
+			this.key_param,
+			this.private_key,
+			getEncoding(JSON.stringify(signed_msg))
+		).then((sig) => {
+			let sig_hex = array2hex(sig);
+			console.log(dest, signed_msg, sig, sig_hex);
+
+			// add the signature to the signed message and send it
+			signed_msg.sig = sig_hex;
+			this.socket.emit(dest, signed_msg);
+		});
 	}
 
-	if (player.seq >= 0 && msg.seq != player.seq + 1)
+	// register a callback for a socket event, with the vai
+	on(dest, callback)
 	{
-		console.log("SEQ MISMATCH", player.seq + 1, msg);
-		return async_false();
+		this.socket.on(dest, (msg) =>
+			this.validate_message(msg).then((status) => callback(status,msg.msg))
+		);
 	}
 
-	let signed_msg = JSON.stringify({
-		msg: msg.msg,
-		seq: msg.seq,
-		key: msg.key,
-	});
-	console.log("signed=", signed_msg);
+	// check the sequence number and signature for an incoming message
+	// to verify that it came from the player that claims to have sent it.
+	async validate_message(msg)
+	{
+		let player = this.players[msg.key];
+		let status = {
+			valid: false,
+			key: false,
+			seq: false,
+			sig: false,
+		};
 
-	return window.crypto.subtle.verify(
-		key_param,
-		player.key,
-		hex2array(msg.sig),
-		getEncoding(signed_msg)
-	).then((valid) => {
-		if (valid)
+		// if we do not have a corresponding key for this player,
+		// then we can't check the sequence number of signature, so
+		// we are done here.
+		if (!player)
 		{
-			player.seq = msg.seq;
-		} else {
-			player.cheats++;
+			console.log("UNKNOWN PLAYER:", msg);
+			return status;
 		}
 
-		player_update(player.id);
-		return valid;
-	});
-;
-}
+		// the key claims to be from a known player
+		status.key = msg.key;
 
+		let signed_msg = JSON.stringify({
+			msg: msg.msg,
+			seq: msg.seq,
+			key: msg.key,
+		});
+		console.log("signed=", signed_msg);
 
-// attempt to validate the signature on the message
-// todo: check sequence number, match to existing player
-socket.on('chat', (msg) => recv_signed(msg).then((valid) => {
-	var item = document.createElement('li');
-	if (!valid)
-	{
-		item.textContent = '❌ ' + msg.msg;
-	} else {
-		item.textContent = '✓ ' + msg.msg;
+		return window.crypto.subtle.verify(
+			this.key_param,
+			player.key,
+			hex2array(msg.sig),
+			getEncoding(signed_msg)
+		).then((valid) => {
+			// store the 
+			status.sig = valid;
+
+			// trust on first use for sequence number,
+			// otherwise require an exact match for the expected value
+			if (player.seq < 0 || msg.seq == player.seq + 1)
+			{
+				status.seq = true;
+			} else {
+				console.log("SEQ MISMATCH", player.seq + 1, msg);
+				valid = false;
+			}
+
+			if (valid)
+			{
+				// seq, sig, and key all match
+				status.valid = true;
+				player.seq = msg.seq;
+			} else {
+				// something didn't match
+				player.cheats++;
+			}
+
+			return status;
+		});
 	}
 
-	messages.appendChild(item);
-	window.scrollTo(0, document.body.scrollHeight);
-}));
+	// when we reconnect to the server, send our public key
+	send_key()
+	{
+		if (this.public_key)
+		{
+			console.log("RECONNECTED");
+			this.socket.emit('register', public_key);
+			return;
+		}
 
-function player_update(id)
-{
-	document.getElementById("player-sequence-"+id).textContent = players[id].seq;
-}
+		console.log("FIRST CONNECT");
+		window.crypto.subtle.generateKey(
+			this.key_param,
+			true,
+			["sign", "verify"]
+		).then((k) => {
+			console.log("Key created", k);
+			this.private_key = k.privateKey;
+			return window.crypto.subtle.exportKey('jwk', k.publicKey);
+		}).then((jwk) => {
+			this.public_key = jwk;
+			this.public_name = jwk2id(jwk);
+			this.socket.emit('register', jwk);
+		});
+	}
 
-socket.on('players', (new_player_list) => {
 	// update the list of players
 	// todo: if there is a game in
 	// action we need to signal an abnormal exit since no new players
 	// should join or current players should leave
 	// todo: detect a player leaving during game play
-
-	let player_list = document.getElementById('players');
-	console.log(new_player_list);
-
-	let new_players = {};
-	for(let new_player of new_player_list)
+	update_players(new_player_list)
 	{
-		// don't trust the server's hash; do it ourselves
-		let id = jwk2id(new_player);
-		new_player.id = id;
-		new_player.seq = -1;
-		new_player.cheats = 0;
-		new_players[id] = new_player;
+		let player_list = document.getElementById('players');
+		console.log(new_player_list);
+
+		let new_players = {};
+		for(let new_player of new_player_list)
+		{
+			// don't trust the server's hash; do it ourselves
+			let id = jwk2id(new_player);
+			new_player.id = id;
+			new_player.seq = -1;
+			new_player.cheats = 0;
+			new_players[id] = new_player;
+		}
+
+		// check to see if any player id's have gone away
+		for(let id in this.players)
+		{
+			if (id in new_players)
+				continue;
+
+			// todo: cancel a game in progress
+			delete players[id];
+			//removeElement("player-"+id);
+		}
+
+		// add in any new player id's
+		for(let id in new_players)
+		{
+			if (id in this.players)
+				continue;
+
+			let player = new_players[id];
+
+			// new player has joined; import their public key
+			// todo: add them in observer mode
+			this.players[id] = player;
+			window.crypto.subtle.importKey(
+				'jwk',
+				player,
+				this.key_param,
+				true,
+				player.key_ops // [ 'verify' ]
+			).then((player_pub) => {
+				player.key = player_pub;
+			});
+
+/*
+			// add them to the player list
+			let it = document.createElement('li');
+			it.setAttribute("id", "player-" + id);
+
+			let seq = document.createElement('span');
+			seq.setAttribute("class", "player-sequence");
+			seq.setAttribute("id", "player-sequence-" + id);
+			seq.textContent = '--';
+			let id_name = document.createElement('span');
+			id_name.setAttribute("class", "player-name");
+			id_name.textContent = id;
+
+			it.appendChild(seq);
+			it.appendChild(id_name);
+			player_list.appendChild(it);
+*/
+		}
+
+/*
+		// update the total player count
+		let counter = document.getElementById('playerCount');
+		counter.textContent = Object.keys(players).length;
+*/
 	}
+}
 
-	// check to see if any player id's have gone away
-	for(let id in players)
-	{
-		if (id in new_players)
-			continue;
-
-		// todo: cancel a game in progress
-		delete players[id];
-		removeElement("player-"+id);
-	}
-
-	// add in any new player id's
-	for(let id in new_players)
-	{
-		if (id in players)
-			continue;
-
-		let player = new_players[id];
-
-		// new player has joined; import their public key
-		// todo: add them in observer mode
-		players[id] = player;
-		window.crypto.subtle.importKey(
-			'jwk',
-			player,
-			key_param,
-			true,
-			player.key_ops // [ 'verify' ]
-		).then((player_pub) => {
-			player.key = player_pub;
-		});
-
-		// add them to the player list
-		let it = document.createElement('li');
-		it.setAttribute("id", "player-" + id);
-
-		let seq = document.createElement('span');
-		seq.setAttribute("class", "player-sequence");
-		seq.setAttribute("id", "player-sequence-" + id);
-		seq.textContent = '--';
-		let id_name = document.createElement('span');
-		id_name.setAttribute("class", "player-name");
-		id_name.textContent = id;
-
-		it.appendChild(seq);
-		it.appendChild(id_name);
-		player_list.appendChild(it);
-	}
-
-	// update the total player count
-	let counter = document.getElementById('playerCount');
-	counter.textContent = Object.keys(players).length;
-});
