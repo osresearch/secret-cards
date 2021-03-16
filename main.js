0e416b36766851b644defd7fc5dc6bd89fc726e5
Trammell hudson <hudson@trmm.net>
2021-03-16 17:56:54 +0100

    move scripts to after DOM

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 "use strict"
 
+// Create a secure channel to communicate with our peers
 let channel = new SecureChannel();
+
+// Setup the chat box to talk to our peers
+let messages = document.getElementById('messages');
+let form = document.getElementById('form');
+let input = document.getElementById('input');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	if (!input.value)
+		return;
+
+	channel.emit('chat', input.value);
+	input.value = '';
+});
 
 function removeElement(id)
 {
@@ -9,24 +24,9 @@ function removeElement(id)
 		it.remove();
 }
 
-channel.on('chat', (status,msg) => {
-	// setup the chat box
-	let messages = document.getElementById('messages');
-	let form = document.getElementById('form');
-	let input = document.getElementById('input');
-
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
-		if (!input.value)
-			return;
-
-		channel.emit('chat', input.value);
-		input.value = '';
-	});
-});
-
-// attempt to validate the signature on the message
-// todo: check sequence number, match to existing peer
+// when a peer sends a chat message, the secure channel
+// wil validate the signature.  we display it either way,
+// with an error if the peer is unknown, out of sequence or bad signature
 channel.on('chat', (status,msg) => {
 	var item = document.createElement('li');
 	if (!status.valid)
