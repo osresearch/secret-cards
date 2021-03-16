850d0f64a18a4a9a5088e7d7c05533446a2bfdd7
Trammell hudson <hudson@trmm.net>
2021-03-16 22:06:58 +0100

    cardtable: handle all the way to wrapping the dealt card

--- cardtable.js
+++ cardtable.js
@@ -32,6 +32,10 @@
  */
 "use strict";
 
+function make_words(x) {
+	return x.map(i => words.bigint2words(BigInt("0x" + i), 4));
+}
+
 /*
  * TODO: Cut-n-choose shuffle protocol
  * TODO: Detect game ending events (players going away)
@@ -42,29 +46,33 @@ class CardTable
 constructor(channel)
 {
 	this.channel = channel;
-	this.players = channel.peers;
+	this.player = this.channel.public_name; // who am i?
+	this.players = []; // will be fixed when the shuffle happens
 	this.deck = null;
 	this.final_deck = null;
 	this.final_player = null;
 	this.prev_player = null;
 	this.drawn_card = null;
 
-	channel.on('draw', (status,msg) => this.draw_msg(status,msg));
-	channel.on('shuffle', (status,msg) => this.shuffle_msg(status,msg));
+	this.channel.on('shuffle', (status,msg) => this.shuffle_msg(status,msg));
+	this.channel.on('draw', (status,msg) => this.draw_msg(status,msg));
+	this.channel.on('wrap', (status,msg) => this.wrap_msg(status,msg));
 }
 
-shuffle(num_cards=52)
+shuffle(num_cards=8)
 {
-	console.log("Starting shuffle operation...");
-
 	// publish a set of proposed cards,
 	// and an ordering of the players to
 	// complete the shuffle
+	// todo: validate that the initial deck we get back is the one we sent
 	this.initial_deck = new_deck(); // not encrypted, everyone validates it
-	this.order = utils.shuffle(Object.keys(this.channel.peers));
+	this.order = Object.keys(this.channel.peers);
+	utils.shuffle(this.order)
+
+	console.log("Starting shuffle operation...", make_words(this.order));
 
 	this.channel.emit('shuffle', {
-		deck: this.initial_deck,
+		deck: Object.values(this.initial_deck),
 		order: this.order,
 		pass: 0,
 	});
@@ -72,10 +80,11 @@ shuffle(num_cards=52)
 
 draw_random_card(deck)
 {
-	for(let c of deck)
+	for(let i in deck)
 	{
+		let c = deck[i];
 		if (!c.played)
-			return c.name;
+			return c.final_name;
 	}
 
 	return null;
@@ -86,20 +95,24 @@ draw_card(name=null)
 	if (name == null)
 	{
 		// find a random card that is not already played
-		name = draw_random_card(this.final_deck);
+		name = this.draw_random_card(this.final_deck);
 		if (name == null)
+		{
+			console.log("EMPTY DECK?");
 			return;
+		}
 	}
 
 	// todo: validate that the card we eventually receive is the right one
 	this.drawn_card = name;
-	const card_name = utils.bigint2hex(name, 64);
+	const card_name = utils.bigint2hex(name, 32);
 
+	console.log("DRAWING", name);
 	this.channel.emit('draw', {
-		dest: this.channel.public_name,
+		dest: this.player,
 		source: this.final_player,
 		final_name: card_name,
-		name: null,
+		name: card_name,
 		nonce: null,
 	});
 }
@@ -111,7 +124,7 @@ draw_msg(status,msg)
 
 	console.log("draw", msg);
 
-	const card = this.final_deck[msg.name];
+	const card = this.final_deck[msg.final_name];
 	if (!card)
 	{
 		console.log("UNKNOWN CARD", msg);
@@ -125,92 +138,159 @@ draw_msg(status,msg)
 		// be cheating if this is an attempt to draw the card again.
 		if (card.player)
 			console.log("CARD ALREADY PLAYED", msg);
+		console.log("Dealing to ", make_words([msg.dest]));
 		card.player = msg.dest;
+		card.known_name = BigInt("0x" + msg.final_name);
+	} else
+	if (!card.known_name)
+	{
+		console.log("deal out of order? no nonces known", card);
+		return;
 	} else {
 		// this is in the chain, so validate the hash and update the deck
 		const nonce = BigInt("0x" + msg.nonce);
 		const name = BigInt("0x" + msg.name);
 		const full_card = nonce << 256n | name;
 		const next_name = utils.sha256bigint(full_card, 64);
-		if (next_name != card.name)
+		if (next_name != card.known_name)
 		{
-			console.log("BAD NONCE", msg);
+			console.log("BAD NONCE", next_name, card.known_name, card, msg);
 			return;
 		}
-		card.name = name;
+		card.known_name = name;
 		card.nonces.push(nonce);
+		console.log("card", msg.final_name, card.nonces);
 	}
 
-	if (msg.source != channel.public_name)
-		return;
-
-	if (msg.dest == channel.public_name)
+	if (msg.source != this.player)
 	{
-		// this is destined for us
+		// nothing else for us to do with this one until
+		// they ask us for it.
+		return;
 	}
+
+	if (msg.dest == this.player)
+		return this.draw_phase2(msg, card);
 
 	// todo: ensure that dest is before me in the order
 	// this one is for me to decrypt and fill in
-	if (msg.dest != channel.public_name)
+	const my_card = this.deck.deck[msg.name];
+	console.log("my card", my_card);
+	if (!my_card)
 	{
-		const card = deck[msg.name];
-		if (!card)
-		{
-			console.log("UNKNOWN CARD", msg);
-			return;
-		}
-
-		if (card.played)
-		{
-			console.log("PLAYED CARD", msg);
-			return;
-		}
-
-		const nonce = BigInt("0x" + msg.nonce);
-		const full_card = nonce << 256n | card.name;
-			let name = utils.sha256bigint(full_card, 64); // 2 * 32 bytes for each hash
-		channel.emit('draw', {
-			dest: msg.msg.dest,
-			source: prev_player,
-			name: "foo",
-		});
+		console.log("UNKNOWN CARD", msg);
+		return;
 	}
+
+	if (my_card.played)
+	{
+		console.log("PLAYED CARD", msg);
+		return;
+	}
+
+	console.log("passing on nonce", my_card);
+	this.channel.emit('draw', {
+		dest: msg.dest,
+		final_name: msg.final_name,
+		name: utils.bigint2hex(my_card.prev_name, 32),
+		nonce: utils.bigint2hex(my_card.nonce, 16),
+		source: this.prev_player,
+	});
 }
 
+draw_phase2(msg,card)
+{
+	// this is destined for us, switch to phase 2 of the draw
+
+	// we should be able to identify it in our deck
+	let card_name = utils.bigint2hex(card.known_name, 32);
+	if (!(card_name in this.deck.deck))
+	{
+		console.log("card not in our deck?", card_name, card);
+		return;
+	}
+
+	card.our_card = this.deck.deck[card_name];
+
+	if (!this.prev_player)
+	{
+		// special case: if we're the first player, then we do not need
+		// to do anything.  it is OURS!
+		let orig_name = utils.bigint2hex(card.our_card.prev_name, 32);
+		card.orig_card = this.initial_deck[orig_name];
+		card.nonces.push(card.our_card.nonce);
+		console.log("FOR US!", orig_name, card);
+		return;
+	}
+
+	console.log("Wrapping", card);
+
+	// generate a temporary key and encrypt the card with it
+	if (card.temp_key)
+		console.log("OVERLAPPING DEALS!");
+	card.temp_key = sra.SRA();
+
+	let reencrypted = card.temp_key.encrypt(card.known_name);
+	this.channel.emit('wrap', {
+		dest: msg.dest,
+		final_name: msg.final_name,
+		source: this.prev_player,
+		encrypted: utils.bigint2hex(reencrypted, 80),
+	});
+}
+
+wrap_msg(status,msg)
+{
+	console.log("WRAP", msg);
+}
 
 /*
  * TODO: validate that we are not in a game!
  */
 shuffle_msg(status,msg)
 {
+	// hack to update our name now that we have one
+	this.player = this.channel.public_name; // who am i?
+
 	if (!status.valid)
 		return;
 
-	console.log("shuffle", msg.pass, status.peer.name);
+	console.log("shuffle", msg.pass, make_words(msg.order));
 
 	// always validate the initial deck
-	if (msg.pass == 0 && !new_deck_validate(msg.deck))
+	if (msg.pass == 0)
+		this.initial_deck = new_deck_validate(msg.deck);
+	if (!this.initial_deck)
+	{
+		console.log("No valid initial deck!");
 		return;
+	}
 
 	// once all the passes are over, the final deck is ready
 	// todo: validate that the deck only has hashes
 	if (msg.pass == msg.order.length)
 	{
-		this.final_deck = msg.deck.map(c => {
-			return {
-				final_name: c.name,
+		this.final_deck = {};
+		for(let c of msg.deck)
+		{
+			if (c.name in this.final_deck)
+				console.log("DUPLICATE CARD!", c);
+
+			this.final_deck[c.name] = {
+				final_name: BigInt("0x" + c.name),
 				nonces: [],
 				name: null,
 				value: null,
 				played: false,
 			};
-		});
+		}
+
 		this.final_player = msg.order[msg.pass - 1];
 		console.log("FINAL DECK", status.peer.name, this.final_deck);
 	}
 
 	// if we are not the shuffler for this round, we're done
-	if (msg.order[msg.pass] != this.channel.public_name)
+	if (msg.order[msg.pass] != this.player)
 		return;
 
 	// todo: validate that there was an initial pass
@@ -221,7 +301,12 @@ shuffle_msg(status,msg)
 	// todo: implement cut-n-choose protocol to ensure fairness
 
 	this.deck = new Deck(msg.deck);
-	this.prev_player = msg.key;
+	this.players = msg.order;
+
+	if (msg.pass == 0)
+		this.prev_player = null;
+	else
+		this.prev_player = status.peer.id;
 
 	console.log("my turn to shuffle", this.deck);
 
@@ -259,7 +344,7 @@ class Deck
 				name: name,
 			};
 
-			this.deck[name] = card;
+			this.deck[utils.bigint2hex(name, 32)] = card;
 		}
 	}
 
@@ -286,26 +371,27 @@ class Deck
 /*
  * Generate a clean deck, in order, with new nonces.
  */
-function new_deck(size=52)
+function new_deck(size=8)
 {
-	let deck = [];
+	let deck = {}
 	for(let i = 0 ; i < size ; i++)
 	{
 		let nonce = utils.randomBigint(256); // bits
 		let full_card = nonce << 256n | BigInt(i);
 		let name = utils.sha256bigint(full_card, 64); // 2 * 32 bytes for each hash
+		let hex_name = utils.bigint2hex(name, 32);
 
-		deck.push({
-			name: utils.bigint2hex(name,32),
+		deck[hex_name] = {
+			name: hex_name,
 			encrypted: utils.bigint2hex(full_card,64),
-		});
+		};
 	}
 
 	return deck;
 }
 
 
-function new_deck_validate(deck, deck_size=52)
+function new_deck_validate(deck, deck_size=8)
 {
 	// validate that the cards are proper
 	const new_deck_size = deck.length;
@@ -315,23 +401,32 @@ function new_deck_validate(deck, deck_size=52)
 		return false;
 	}
 
+	let new_deck = {};
+
 	for(let i = 0 ; i < deck_size ; i++)
 	{
 		const card = deck[i];
 		const value = BigInt("0x" + card.encrypted);
 		const name = BigInt("0x" + card.name);
 		const mask = (1n << 256n) - 1n;
-		if ((value & mask) == BigInt(i))
-			continue;
-		console.log("card " + i + " invalid value:", card);
-
+		const card_value = value & mask;
 		const hash_name = utils.sha256bigint(value, 64); // 2 * 32 bytes for each hash
-		if (hash_name == name)
-			continue;
-		console.log("card " + i + " invalid hash:", card);
-		return false;
+
+		if (card_value != BigInt(i))
+		{
+			console.log("card " + i + " invalid value:", card);
+			return;
+		}
+
+		if (hash_name != name)
+		{
+			console.log("card " + i + " invalid hash:", card);
+			return;
+		}
+
+		new_deck[card.name] = deck[i];
 	}
 
-	console.log("Initial deck validated");
-	return true;
+	console.log("Initial deck validated", new_deck);
+	return new_deck;
 }
