8f81f0dfaf9f6be15419e8d687e9590ad93a2197
Trammell hudson <hudson@trmm.net>
2021-03-22 22:38:52 +0100

    index: use callbacks to update display

--- index.js
+++ index.js
@@ -32,16 +32,70 @@ channel.on('chat', (status,msg) => {
 	var item = document.createElement('li');
 	if (!status.valid)
 	{
-		item.textContent = '❌ ' + msg + " " + status;
+		item.textContent = '❌ ' + peer.name + ": " + msg + " " + status;
 	} else {
-		item.textContent = '✓ ' + msg;
+		item.textContent = '✓ ' + peer.name + ": " + msg;
 	}
 
 	messages.appendChild(item);
 	window.scrollTo(0, document.body.scrollHeight);
 });
 
-function peer_update(id)
+channel.peer_new = (peer) =>
 {
-	document.getElementById("peer-sequence-"+id).textContent = peers[id].seq;
+	let peer_list = document.getElementById('peers');
+	if (!peer_list)
+		return;
+
+	let it = document.createElement('li');
+	it.setAttribute("id", "peer-" + peer.id);
+
+	let seq = document.createElement('span');
+	seq.setAttribute("class", "peer-sequence");
+	seq.setAttribute("id", "peer-sequence-" + peer.id);
+	seq.textContent = '-- ';
+
+	let id_name = document.createElement('span');
+	id_name.textContent = peer.name;
+	if (peer.id == channel.public_name)
+		id_name.setAttribute("class", "player-name");
+	else
+		id_name.setAttribute("class", "peer-name");
+
+
+	it.appendChild(seq);
+	it.appendChild(id_name);
+	peer_list.appendChild(it);
+
+	let peers = document.getElementById('peer-count');
+	peers.textContent = Object.keys(channel.peers).length;
+}
+
+channel.peer_remove = (peer) =>
+{
+	let it = document.getElementById("peer-" + peer.id);
+	if (it)
+		it.remove();
+
+	if (cards.ready)
+	{
+		console.log("GAME OVER! Player disconnected", peer.name);
+		cards.ready = false;
+	}
+
+	document.getElementById('peer-count').textContent = channel.peers.length;
+}
+
+channel.update = (dest,status,msg) =>
+{
+	// update the sequence number for this peer
+	// as well as flag any cheating attempts
+	let seq = document.getElementById("peer-sequence-" + status.peer.id);
+	if (!seq)
+		return;
+
+	if (!status.valid)
+		seq.setAttribute("cheater", true);
+
+	seq.textContent = msg.seq + ' ';
 }
